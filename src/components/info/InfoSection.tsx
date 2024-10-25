a8c5c217548e8ad0b910fe7fe8fa1b1926c46688
marcio.pereira <marcio.pereira@usc.unirede.net>
2024-10-24 21:37:27 -0300

    add import styles

--- src/components/info/InfoSection.tsx
+++ src/components/info/InfoSection.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './styles.css';
 
-const FeaturesComponent: React.FC = () => {
+const InfoSection: React.FC = () => {
   return (
-    <div className="container">
-      <div className="featureGroup">
+    <div className="infoSection container">
+      <div className="infoSection featureGroup">
         {features.map((feature, index) => (
-          <div key={index} className="featureItem">
-            <img src={feature.icon} alt={feature.title} className="icon" />
-            <div className="textContainer">
+          <div key={index} className="infoSection featureItem">
+            <img src={feature.iconInfo} alt={feature.title} className="icon-info" />
+            <div className="infoSection textContainer">
               <div className="title">{feature.title}</div>
               <div className="subtitle">{feature.subtitle}</div>
             </div>
@@ -20,25 +21,25 @@ const FeaturesComponent: React.FC = () => {
 
 const features = [
   {
-    icon: './assets/trophy-1.svg',
+    iconInfo: './assets/trophy-1.svg',
     title: 'High Quality',
     subtitle: 'crafted from top materials',
   },
   {
-    icon: './assets/guarantee.svg',
+    iconInfo: './assets/guarantee.svg',
     title: 'Warranty Protection',
     subtitle: 'Over 2 years',
   },
   {
-    icon: './assets/shipping.svg',
+    iconInfo: './assets/shipping.svg',
     title: 'Free Shipping',
     subtitle: 'Order over 150 $',
   },
   {
-    icon: './assets/customer-support.svg',
+    iconInfo: './assets/customer-support.svg',
     title: '24 / 7 Support',
     subtitle: 'Dedicated support',
   },
 ];
 
-export default FeaturesComponent;
+export default InfoSection;
